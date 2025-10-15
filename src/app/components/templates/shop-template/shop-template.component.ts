a93bb5e8eac4df7a834356dc2b1722f6e0237765
Jhony Quintero <54184615+gabo0130@users.noreply.github.com>
2025-10-15 13:46:31 -0500

    chore: initial commit of Angular frontend

---
+++ src/app/components/templates/shop-template/shop-template.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-template',
+  standalone: true,
+  imports: [],
+  templateUrl: './shop-template.component.html',
+  styleUrl: './shop-template.component.scss'
+})
+export class ShopTemplateComponent {
+
+}
